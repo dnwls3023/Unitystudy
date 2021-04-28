22a6a6da847c5fa7e53ebcb2e1a9b37419da5db7
dnwls3023 <68366527+dnwls3023@users.noreply.github.com>
2021-04-28 20:02:31 +0900

    Update PlayerBall.cs

--- PlayerBall.cs
+++ PlayerBall.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerBall : MonoBehaviour
 {
+  Rigidbody rigid;
   void Awake()
   {
-    
+    rigid = GetComponent<Rigidbody>();
   }
   // 물리 기반으로 움직일 것이므로
   void FixedUpdate()
   {
+    float h = Input.GetAxisRaw("Horizontal");
+    float v = Input.GetAxisRaw("Vertical");
+    
+    rigid.AddForce(new Vector3(h,0,v), ForceMode.Impulse);
     
   }
 }
