5bb0067dd2739eeff4cd9a924c630cfeab1a0811
dnwls3023 <68366527+dnwls3023@users.noreply.github.com>
2021-04-28 19:54:20 +0900

    Create PlayerBall.cs

---
+++ PlayerBall.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+  void Awake()
+  {
+    
+  }
+  // 물리 기반으로 움직일 것이므로
+  void FixedUpdate()
+  {
+    
+  }
+}
